b7199e51ad013d7ca8a53edbf875b609b4a6ede8
Pavel-Notebook <lobachev-2019@mail.ru>
2024-08-07 19:03:03 +0500

    add global widgets

--- components/pages/main.vue
+++ components/pages/main.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
     <div>
-        
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  components: {
+    global: true,
+    dirs: ['~/components/widgets']
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
